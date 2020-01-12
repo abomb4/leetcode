ad80b1a79e3d24de0c45561386de43256e15000a
杨睿龙 <yangrl14628@hundsun.com>
2020-01-12 15:13:33 +0800

    1_twosum

--- 1_twosum/src/main/java/Solution.java
+++ 1_twosum/src/main/java/Solution.java
@@ -1,9 +1,39 @@
 import java.util.*;
 
+import static com.abomb4.leetcode.test.Asserts.assertEquals;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author abomb4 2020-01-12
+ */
 public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
-        return new int[]{};
+        final HashMap<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            final int num = nums[i];
+            final int need = target - num;
+            final Integer oldIndex = map.get(need);
+            if (oldIndex != null) {
+                return new int[]{oldIndex, i};
+            }
+            map.put(num, i);
+        }
+        throw new IllegalArgumentException("Not found");
     }
 
     public static void main(String[] args) {
@@ -12,15 +42,5 @@ public class Solution {
         assertEquals(2, result.length, "result.length");
         assertEquals(0, result[0], "result[0]");
         assertEquals(1, result[1], "result[1]");
-    }
-
-    private static void assertEquals(Object expected, Object target, String info) {
-        if (!isEquals(expected, target)) {
-            throw new RuntimeException("【"+info+"】目标 ["+target+"] 与期望 ["+expected+"] 不符");
-        }
-    }
-
-    private static boolean isEquals(Object expected, Object target) {
-        return (expected == target || Objects.equals(expected, target));
     }
 }
