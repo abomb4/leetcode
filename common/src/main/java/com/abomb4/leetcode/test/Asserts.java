b1b5c80ab96a47d1ad5c76f7b11b8b47b19f324b
杨睿龙 <yangrl14628@hundsun.com>
2020-01-12 19:25:34 +0800

    739_daily-temperatures

--- common/src/main/java/com/abomb4/leetcode/test/Asserts.java
+++ common/src/main/java/com/abomb4/leetcode/test/Asserts.java
@@ -1,5 +1,6 @@
 package com.abomb4.leetcode.test;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
@@ -8,18 +9,52 @@ import java.util.Objects;
 public class Asserts {
 
     public static void assertEquals(Object expected, Object target, String info) {
-        if (!isEquals(expected, target)) {
-            throw new RuntimeException("【"+info+"】目标 ["+target+"] 与期望 ["+expected+"] 不符");
+        if (isNotEquals(expected, target)) {
+            throw new RuntimeException("【" + info + "】目标 [" + target + "] 与期望 [" + expected + "] 不符");
+        }
+    }
+
+    public static void assertArrayEquals(int[] expected, int[] target, String info) {
+        if (expected == null || target == null) {
+            throw new RuntimeException("【" + info + "】两个 array 中存在 null");
+        }
+        if (isNotEquals(expected.length, target.length)) {
+            throw new RuntimeException("【" + info + "】目标 " + Arrays.toString(target)
+                    + " 与期望 " + Arrays.toString(expected) + " 长度不符");
+        }
+
+        for (int i = 0; i < expected.length; i++) {
+            if (isNotEquals(expected[i], target[i])) {
+                throw new RuntimeException("【" + info + "】目标 " + Arrays.toString(target)
+                        + " 与期望 " + Arrays.toString(expected) + " 的第 [" + i + "] 项不符");
+            }
+        }
+    }
+
+    public static <T> void assertArrayEquals(T[] expected, T[] target, String info) {
+        if (expected == null || target == null) {
+            throw new RuntimeException("【" + info + "】两个 array 中存在 null");
+        }
+        if (isNotEquals(expected.length, target.length)) {
+            throw new RuntimeException("【" + info + "】目标 " + Arrays.toString(target)
+                    + " 与期望 " + Arrays.toString(expected) + " 长度不符");
+        }
+
+        for (int i = 0; i < expected.length; i++) {
+            if (isNotEquals(expected[i], target[i])) {
+                throw new RuntimeException("【" + info + "】目标 " + Arrays.toString(target)
+                        + " 与期望 " + Arrays.toString(expected) + " 的第 [" + i + "] 项不符");
+            }
         }
     }
 
     public static void assertNotNull(Object target, String info) {
         if (target == null) {
-            throw new RuntimeException("【"+info+"】目标 ["+target+"] 是 null");
+            throw new RuntimeException("【" + info + "】目标 [" + target + "] 是 null");
         }
     }
 
-    public static boolean isEquals(Object expected, Object target) {
-        return (expected == target || Objects.equals(expected, target));
+    public static boolean isNotEquals(Object expected, Object target) {
+        return (expected != target && !Objects.equals(expected, target));
     }
 }
