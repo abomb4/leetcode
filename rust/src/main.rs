bf995f1d0479d9391fd8ce528075cc7864c63830
abomb4 <yangrlx@gmail.com>
2020-04-14 16:17:38 +0800

    solution 445

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,16 @@
 mod solutions;
 use solutions::solution3::test as test3;
+use solutions::solution445::test as test445;
 
 fn test(num: i32) {
     match num {
         3 => test3(),
+        445 => test445(),
         _ => println!("solution {0} not found", num),
     }
 }
 
 fn main() {
-    test(3);
+    test(445);
 }
 
--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -1,2 +1,3 @@
 
 pub mod solution3;
+pub mod solution445;
