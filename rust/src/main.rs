4e03ce50f7e7c77349491e38c619d31480f641e9
abomb4 <yangrlx@gmail.com>
2020-07-25 16:05:30 +0800

    215 and vscode config

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,9 @@
 mod solutions;
 use solutions::solution3::test as test3;
 use solutions::solution11::test as test11;
+use solutions::solution15::test as test15;
 use solutions::solution33::test as test33;
+use solutions::solution215::test as test215;
 use solutions::solution445::test as test445;
 use solutions::solution542::test as test542;
 use solutions::solution974::test as test974;
@@ -10,7 +12,9 @@ fn test(num: i32) {
     match num {
         3 => test3(),
         11 => test11(),
+        15 => test15(),
         33 => test33(),
+        215 => test215(),
         445 => test445(),
         542 => test542(),
         974 => test974(),
@@ -19,5 +23,5 @@ fn test(num: i32) {
 }
 
 fn main() {
-    test(11);
+    test(215);
 }
--- rust/src/solutions/mod.rs
+++ rust/src/solutions/mod.rs
@@ -3,6 +3,7 @@ pub mod solution3;
 pub mod solution11;
 pub mod solution15;
 pub mod solution33;
+pub mod solution215;
 pub mod solution445;
 pub mod solution542;
 pub mod solution974;
